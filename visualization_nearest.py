b059ecbe4f8326faa1fd190d045182a19a372c7a
hzgyy <2050712905@qq.com>
2025-11-12 10:59:38 +0800

    update 11.12

---
+++ visualization_nearest.py
@@ -0,0 +1,245 @@
+import os
+import numpy as np
+
+import utils
+import align_dataset_test as align_dataset
+from config import CONFIG
+
+from evals.phase_classification import evaluate_phase_classification, compute_ap
+from evals.phase_classification import fit_svm,evaluate_svm
+from evals.kendalls_tau import evaluate_kendalls_tau
+from evals.phase_progression import evaluate_phase_progression
+
+from train import AlignNet
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+import random
+import argparse
+import glob
+from natsort import natsorted
+from tqdm import tqdm
+import cv2
+from PIL import Image, ImageDraw, ImageFont
+
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+
+
+def get_embeddings(model, data, labels_npy, args):
+
+    embeddings = []
+    labels = []
+    frame_paths = []
+    names = []
+
+    device = f"cuda:{args.device}"
+    
+    for act_iter in iter(data):
+        for i, seq_iter in enumerate(act_iter):
+            seq_embs = []
+            seq_fpaths = []
+            original = 0
+            for _, batch in enumerate(seq_iter):
+                a_X, a_name, a_frames = batch
+                a_X = a_X.to(device).unsqueeze(0)
+                original = a_X.shape[1]//2
+                
+#                 a_X = a_X[:,:a_X.shape[1]//2,:,:,:]
+#                 original = a_X.shape[1]//2
+                
+#                 if ((args.num_frames*2)-a_X.shape[1]) > 0:
+#                     b = a_X[:, -1].clone()
+#                     b = torch.stack([b]*((args.num_frames*2)-a_X.shape[1]),axis=1).to(device)
+#                     a_X = torch.concat([a_X,b], axis=1)
+                
+                b =  a_X[:, -1].clone()
+                try:
+                    b = torch.stack([b]*((args.num_frames*2)-a_X.shape[1]),axis=1).to(device)
+                except:
+                    b = torch.from_numpy(np.array([])).float().to(device)
+                a_X = torch.concat([a_X,b], axis=1)
+                a_emb = model(a_X)[:, :original,:]
+                
+                if args.verbose:
+                    print(f'Seq: {i}, ', a_emb.shape)
+
+                seq_embs.append(a_emb.squeeze(0).detach().cpu().numpy())
+                seq_fpaths.extend(a_frames)
+            
+            seq_embs = np.concatenate(seq_embs, axis=0)
+            
+            name = str(a_name).split('/')[-1]
+            # name = name[:8] + '/' + name[8:10] + '/' + name[10:]
+            lab = labels_npy[name]['labels']
+            end = min(seq_embs.shape[0], len(lab))
+            lab = lab[:end]#.T
+            seq_embs = seq_embs[:end]
+            print(seq_embs.shape, len(lab))
+            embeddings.append(seq_embs[:end])
+            frame_paths.append(seq_fpaths)
+            names.append(a_name)
+            labels.append(lab)
+
+    return embeddings, names, labels
+
+def make_video_with_captions(imgs, captions, save_path="output.mp4", fps=10, font_path="/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"):
+    """
+    将图像序列和对应文字合成为视频
+    imgs: list of PIL.Image
+    captions: list of str
+    save_path: 输出视频路径
+    fps: 帧率
+    font_path: 字体路径 (Linux默认路径，可换成别的)
+    """
+    assert len(imgs) == len(captions), "imgs 和 captions 长度不一致！"
+    
+    # 转为统一尺寸
+    w, h = imgs[0].size
+    size = (w, h)
+    
+    # 初始化视频写入器
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(save_path, fourcc, fps, size)
+    
+    # 字体
+    try:
+        font = ImageFont.truetype(font_path, size=24)
+    except:
+        font = ImageFont.load_default()
+
+    for img, cap in zip(imgs, captions):
+        # 确保是 RGB 格式
+        img = img.convert("RGB")
+        draw = ImageDraw.Draw(img)
+        
+        # 在底部绘制半透明背景
+        text_w, text_h = draw.textsize(cap, font=font)
+        margin = 10
+        overlay_height = text_h + 2 * margin
+        draw.rectangle(
+            [(0, h - overlay_height), (w, h)],
+            fill=(0, 0, 0, 128)
+        )
+
+        # 写入文字（白色）
+        draw.text((margin, h - text_h - margin), cap, fill=(255, 255, 255), font=font)
+        
+        # 转换为 OpenCV 格式
+        frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        out.write(frame)
+    
+    out.release()
+    print(f"✅ 视频已保存至 {save_path}")
+
+
+def get_embs(traj_path,model,_transforms,args,device):
+    img_paths = natsorted(glob.glob(os.path.join(traj_path, '*.jpg')))
+    imgs = utils.get_pil_images(img_paths)
+    print(len(img_paths))
+    imgs = _transforms(imgs)
+    print(imgs.shape)
+    
+    # get embeddings
+    a_X = imgs.to(device).unsqueeze(0)
+    original = a_X.shape[1]//2
+    a_emb = model(a_X)
+    print(f"emb shape:{a_emb.shape},original:{original}")
+    a_emb = a_emb[:, :original,:]
+
+    a_emb_reduced = a_emb.squeeze(0).detach().cpu().numpy()
+    return a_emb_reduced
+
+def knn_cosine_estimate(embs_all, labels_all, test_embs, K):
+    # 1. L2 normalization
+    embs_all_norm = embs_all / np.linalg.norm(embs_all, axis=1, keepdims=True)  # (N,128)
+    test_embs_norm = test_embs / np.linalg.norm(test_embs, axis=1, keepdims=True)  # (M,128)
+
+    # 2. Compute cosine similarity (M,N)
+    # sim[i,j] = dot(test_embs_norm[i], embs_all_norm[j])
+    sim = test_embs_norm @ embs_all_norm.T   # (M,N)
+
+    # 3. Take top K nearest neighbors by similarity
+    knn_indices = np.argpartition(-sim, K, axis=1)[:, :K]   # (M,K)
+
+    # 4. Average the K labels
+    estimated = np.mean(labels_all[knn_indices], axis=1)    # (M,)
+
+    return estimated
+
+def main(ckpt,args):
+    device = f"cuda:{args.device}"
+    # device = "cpu"
+    # get trained model
+    # _, ckpt_step = ckpt.split('.')[0].split('_')[-2:]
+    # ckpt_step = int(ckpt_step.split('=')[1])
+    # DEST = os.path.join(args.dest, 'eval_step_{}'.format(ckpt_step))
+    model = AlignNet.load_from_checkpoint(ckpt, map_location=device)
+    # model.hparams.config.DATA.NUM_CONTEXT = 1
+    model.to(device)
+    model.eval()
+    # grad off
+    torch.set_grad_enabled(False)
+    
+    # read in images:
+    if args.data_path == None:
+        data_path = '/root/autodl-tmp/LAV-CVPR21/test/pouring2'
+    else:
+        data_path = args.data_path
+    if os.path.exists(args.root) == False:
+        os.mkdir(args.root)
+    #preprarations
+    _transforms = utils.get_transforms(augment=False)
+    labels_all = []
+    embs_all = []
+    trajs_path = glob.glob(os.path.join(data_path,'vid*'))
+    #num_val_trajs = 10-num_train_trajs
+
+    for traj_path in tqdm(trajs_path[:-1]):
+        # read images
+        # trajs_path = os.path.join(data_path,f'vid{i}')
+        # print(trajs_path)
+        a_emb_reduced = get_embs(traj_path,model,_transforms,args,device)
+        
+        embs_all.append(a_emb_reduced)
+        labels = np.arange(a_emb_reduced.shape[0])/a_emb_reduced.shape[0]
+        print(labels)
+        labels_all.append(labels)
+    print(embs_all[0].shape,labels_all[0].shape)
+    embs_all = np.concatenate(embs_all,axis=0)
+    labels_all = np.concatenate(labels_all,axis=0)
+    #test
+    test_embs = get_embs(trajs_path[-1],model,_transforms,args,device)
+    estimated = knn_cosine_estimate(embs_all,labels_all,test_embs,3)
+    print(estimated,estimated.shape)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, required=True)
+    parser.add_argument('--data_path', type=str, default=None)
+    parser.add_argument('--batch_size', type=int, default=20)
+    parser.add_argument('--dest', type=str, default='./')
+    parser.add_argument('--description', type=str, default=None)
+    # parser.add_argument('--spec_class', type=int, default=None)
+    parser.add_argument('--root', type=str, default=None)
+
+    parser.add_argument('--stride', type=int, default=5)
+    parser.add_argument('--visualize', dest='visualize', action='store_true')
+    parser.add_argument('--device', type=int, default=0, help='Cuda device to be used')
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--num_frames', type=int, default=None, help='Path to dataset')
+
+    args = parser.parse_args()
+
+    # if os.path.isdir(args.model_path):
+    #     ckpts = natsorted(glob.glob(os.path.join(args.model_path, '*')))
+    # else:
+    #     ckpts = [args.model_path]
+    ckpt = args.model_path
+    
+    ckpt_mul = args.device
+    main(ckpt, args)
